ee1ac54cf0a3d39ec751fe724f16d2a96bcf798b
ayofimihan <cogunsina@gmail.com>
2022-10-18 03:58:59 +0100

    finished basic contract

--- hardhat-tut/contracts/whitelist.sol
+++ hardhat-tut/contracts/whitelist.sol
@@ -3,7 +3,21 @@ pragma solidity ^0.8.7;
 
 contract Whitelist{
     uint8 maxWhiteListedAddresses;
+    uint numWhiteListedAddresses;
+    mapping (address => bool) public whitelistedAddresses;
+
     constructor(uint8 _maxWhiteListedAddresses){
         maxWhiteListedAddresses = _maxWhiteListedAddresses; 
     }
+
+    function addAddressToWhitelist() public {
+        require(!whitelistedAddresses[msg.sender], "you're already whitelisted omo ope");
+        require( numWhiteListedAddresses < maxWhiteListedAddresses, "too late ngmi" );
+        whitelistedAddresses[msg.sender] = true;
+        numWhiteListedAddresses++;
+
+    }
+
+
+
 }
