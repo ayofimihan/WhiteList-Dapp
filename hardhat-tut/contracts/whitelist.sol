4a8c7fcb815ece3bbdc61b2d2a12b1f41df8c523
ayofimihan <cogunsina@gmail.com>
2022-10-18 03:38:38 +0100

    contract bit

--- hardhat-tut/contracts/whitelist.sol
+++
@@ -1,9 +0,0 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
-
-contract Whitelist{
-    uint8 maxWhiteListedAddresses;
-    constructor(uint8 _maxWhiteListedAddresses){
-        maxWhiteListedAddresses = _maxWhiteListedAddresses; 
-    }
-}
