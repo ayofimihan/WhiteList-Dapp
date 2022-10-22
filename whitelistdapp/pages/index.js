9ed1e6cee342cfabdec1a70aa7ea58b0a72f53c8
ayofimihan <cogunsina@gmail.com>
2022-10-22 22:18:10 +0100

    providersigner

--- whitelistdapp/pages/index.js
+++ whitelistdapp/pages/index.js
@@ -2,15 +2,40 @@ import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
+import { providers } from "ethers";
 
 export default function Home() {
   const [numWhiteListedAddresses, setNumWhiteListedAddresses] = useState(0);
   const [walletConnected, setWalletConnected] = useState(false);
   const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    try {
+      const provider = await web3ModalRef.current.connect();
+      const web3Provider = new providers.Web3Provider(provider);
+      const { chainId } = await web3Provider.getNetwork();
+      console.log(chainId);
+      if (chainId !== 5) {
+        alert("switch to goerli fam");
+      }
+
+      if (needSigner) {
+        const signer = web3Provider.getSigner();
+        return signer;
+      }
+
+      return web3Provider;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   const connectWallet = async () => {
     try {
+      await getProviderOrSigner();
       setWalletConnected(true);
-      
+      checkIfAddressWhitelisted();
+      checkNumWhitelisted();
     } catch (err) {
       console.error(err);
       console.log(err);
@@ -35,7 +60,7 @@ export default function Home() {
       </Head>
       <div className={styles.main}>
         <h1 className={styles.title}> California Jacuzzi</h1>
-        
+
         <div className={styles.description}>
           {numWhiteListedAddresses} fuckers have been whitelisted, try your
           luck!
