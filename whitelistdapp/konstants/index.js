2875b8b9821b6bd9d711eb4a6233923afde10e76
ayofimihan <cogunsina@gmail.com>
2022-10-22 23:44:51 +0100

    gn

---
+++ whitelistdapp/konstants/index.js
@@ -0,0 +1,40 @@
+export const CONTRACT_ADDRESS = "0x5684c785bc826005e8f0f97d3a1bf4f50dbc1b4a";
+export const abi = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint8",
+          "name": "_maxWhiteListedAddresses",
+          "type": "uint8"
+        }
+      ],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "inputs": [],
+      "name": "addAddressToWhitelist",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "whitelistedAddresses",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ];
--- whitelistdapp/pages/index.js
+++ whitelistdapp/pages/index.js
@@ -2,11 +2,13 @@ import Head from "next/head";
 import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
-import { providers } from "ethers";
+import { Contract, providers } from "ethers";
+import { abi, CONTRACT_ADDRESS } from "../konstants";
 
 export default function Home() {
   const [numWhiteListedAddresses, setNumWhiteListedAddresses] = useState(0);
   const [walletConnected, setWalletConnected] = useState(false);
+  const [_joinedWhitelist, setJoinedWhitelist] = useState()
   const web3ModalRef = useRef();
 
   const getProviderOrSigner = async (needSigner = false) => {
@@ -25,6 +27,21 @@ export default function Home() {
       }
 
       return web3Provider;
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const checkIfAddressWhitelisted = async () => {
+    try {
+      const signer = getProviderOrSigner(true);
+      const contract = new Contract(CONTRACT_ADDRESS, abi, signer);
+      const address = await signer.getAddress();
+      const _joinedWhitelist = await contract.whitelistedAddresses(address);
+      setJoinedWhitelist(_joinedWhitelist);
+
+
+
     } catch (err) {
       console.log(err);
     }
